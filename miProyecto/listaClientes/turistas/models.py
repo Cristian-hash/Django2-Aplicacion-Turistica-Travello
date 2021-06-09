c0d5594dcbb9b12d5751915570330b30c7aa5960
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-06-09 16:25:30 -0500

    Identificacion turista archivo turista-models.py

--- miProyecto/listaClientes/turistas/models.py
+++ miProyecto/listaClientes/turistas/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+#Creando el modelo 
+
+class Turista(models.Model):
+    nombre=models.TextField()
+    apellidos=models.TextField()
+    dni=models.TextField()
