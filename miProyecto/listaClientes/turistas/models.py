feb2019357d7bc2e357dfd322e63e241405bcb1b
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-06-10 21:18:11 -0500

    Aumento de atributos a Turistas

--- miProyecto/listaClientes/turistas/models.py
+++ miProyecto/listaClientes/turistas/models.py
@@ -6,5 +6,11 @@ from django.db import models
 class Turista(models.Model):
     nombres=models.TextField()
     apellidos=models.TextField()
+
+    #no nos importa de que pais venga
     nacionalidad=models.TextField()
+    #importante que se tenga que marcar esta casilla por eso lo dejo en false
+    vacunado_Covid=models.BooleanField(default=False)
+
+    
 #creacion de un objeto por el shell
