6606e6d49f8e1edee179be75c1a9165959979dfa
Crhistian Ziegler Pacori Paucar <cpacori@unsa.edu.pe>
2021-06-15 11:20:17 -0500

    Pasando los datos dinamicos en html

--- miProyecto/listaClientes/travello/models.py
+++ miProyecto/listaClientes/travello/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+# min 1:25:52
 # Create your models here.
+
+class Destination:
+    id: int
+    name : str
+    img:str
+    desc: str
+    price:int
--- miProyecto/listaClientes/travello/views.py
+++ miProyecto/listaClientes/travello/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 def index(request,*args,**kwargs):
 
+    dest1=Destination()
+    dest1.name='Arequipa (Dinamico)'#cambiando statico a dinamico
+    dest1.desc='Ciudad Blanca(Dinamico)'
+    dest1.price=1000
+    
     #creacion de diccionario 
 
     #de partir de la coma espara convertir el precio en dinamico
-    return render(request,'index.html',{'price':700})
+    return render(request,'index.html',{'dest1':dest1})
